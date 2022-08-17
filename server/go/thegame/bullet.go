d8b095630ae4704a8d7dd00600364c837f595308
square-melon <62755676+square-melon@users.noreply.github.com>
2022-08-17 20:16:28 +0800

    fix

--- server/go/thegame/bullet.go
+++ server/go/thegame/bullet.go
@@ -57,7 +57,7 @@ func (b *Bullet) Team() int {
 func (b *Bullet) BodyDamage() int {
 	x = rand.Intn(100)
 	if x % 25 == 1 {
-		return b.owner.ability(BulletDamage * 20)
+		return b.owner.ability(BulletDamage) * 20
 	}
 	return b.owner.ability(BulletDamage)
 }
