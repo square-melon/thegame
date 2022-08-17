e8a6c6d675da93e450afb38d78401326c4ed2e90
square-melon <62755676+square-melon@users.noreply.github.com>
2022-08-17 15:48:05 +0800

    Update ability.go

--- server/go/thegame/ability.go
+++ server/go/thegame/ability.go
@@ -21,12 +21,12 @@ func linearScale(at0, at8 int) [9]int {
 }
 
 var AbilityValues = [NAbilities][9]int{
-	{0, 0, 1, 2, 3, 4, 5, 6, 7},    // HealthRegen
-	linearScale(1000, 5520),        // MaxHealth
-	linearScale(15, 55),            // BodyDamage
-	linearScale(8, 24),             // BulletSpeed
-	linearScale(40, 136),           // BulletPenetration
-	linearScale(18, 58),            // BulletDamage
-	{15, 13, 11, 9, 7, 6, 5, 4, 3}, // Reload
-	linearScale(6, 14),             // MovementSpeed
+	{0, 1, 1, 2, 2, 3, 3, 7, 8},                                   // HealthRegen
+	{1000, 1500, 2000, 2500, 3000, 3500, 4000, 5200, 6000},        // MaxHealth
+	{15, 18, 22, 27, 31, 35, 40, 50, 58},                          // BodyDamage
+	linearScale(8, 24),                                            // BulletSpeed
+	linearScale(40, 136),                                          // BulletPenetration
+	{18, 19, 20, 21, 28, 35, 42, 52, 62},                          // BulletDamage
+	{15, 14, 12, 9, 7, 6, 5, 4, 2},                                // Reload
+	linearScale(6, 14),                                            // MovementSpeed
 }
